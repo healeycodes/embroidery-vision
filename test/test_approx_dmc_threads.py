f2b878eeac7a7fa54edfcac32fb19e648939681f
Andrew <healeycodes@gmail.com>
2021-04-02 19:12:24 +0100

    Fix test_end_to_end failing

--- test/test_approx_dmc_threads.py
+++ test/test_approx_dmc_threads.py
@@ -6,12 +6,12 @@ class TestApproxThreads(unittest.TestCase):
     def test_end_to_end(self):
         pomegranate = 'test/pomegranate.jpg'
         circle_image, original_image = get_largest_circle(pomegranate, debug=False)
-        thread_palette = generate_palette(circle_image, original_image, output_file=False, debug=False)
+        thread_palette = generate_palette(circle_image, original_image, output_file=False)
 
         self.assertIn('#3857 Rosewood Dark', thread_palette)
 
         computer = 'test/computer.jpg'
         circle_image, original_image = get_largest_circle(computer, debug=False)
-        thread_palette = generate_palette(circle_image, original_image, debug=False)
+        thread_palette = generate_palette(circle_image, original_image)
 
         self.assertIn('#156 Blue Violet Med Lt', thread_palette)
